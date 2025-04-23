6ff404e3cfccd27423c947aef393bf62c1799328
Dastan <dastan.kubanuulu1@gmail.com>
2025-04-23 14:04:23 +0600

    add cdek logic

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'modeltranslation',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -46,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'django.middleware.locale.LocaleMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -185,13 +188,32 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "ru-ru"
+LANGUAGE_CODE = "ru"
+
+
+MODELTRANSLATION_DEFAULT_LANGUAGE = 'ru'
+
+MODELTRANSLATION_PREPOPULATE_LANGUAGE = 'ru'
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale/'),
+)
+
+MODELTRANSLATION_LANGUAGES = ('ru', 'ky')
+
 
 TIME_ZONE = "Asia/Bishkek"
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
+
+LANGUAGES = [
+    ('ru', _('Russian')),
+    ('ky', _('Kyrgyz')),
+]
 
 
 # Static files (CSS, JavaScript, Images)
---
+++ myapp/translation.py
@@ -0,0 +1,63 @@
+from modeltranslation.translator import register, TranslationOptions
+from .models import *
+
+
+@register(News)
+class NewsTranslationOptions(TranslationOptions):
+    fields = ('title', 'mini_title', 'content')
+
+
+@register(Document)
+class ServiceTranslationOptions(TranslationOptions):
+    fields = ('title',)
+
+
+@register(ProjectCategory)
+class AdvantageTranslationOptions(TranslationOptions):
+    fields = ('name', )
+
+
+@register(Project)
+class CategoryTranslationOptions(TranslationOptions):
+    fields = ('title', 'description', )
+
+
+@register(NewsCategory)
+class TestimonialTranslationOptions(TranslationOptions):
+    fields = ('name', )
+
+
+@register(PublicationCategory)
+class EmployeeTranslationOptions(TranslationOptions):
+    fields = ('name', )
+
+
+@register(Publication)
+class BannerTranslationOptions(TranslationOptions):
+    fields = ('title', 'description')
+
+
+@register(AboutUs)
+class SiteSettingTranslationOptions(TranslationOptions):
+    fields = ('title', 'description', 'description2', 'description3')
+
+
+@register(Page)
+class ProductTranslationOptions(TranslationOptions):
+    fields = ('title', 'description')
+
+
+@register(MorePage)
+class CategoryVacancyTranslationOptions(TranslationOptions):
+    fields = ('title', 'mini_title', 'description')
+
+
+@register(PageImage)
+class VacancyTranslationOptions(TranslationOptions):
+    fields = ('name',)
+
+
+@register(GalleryImage)
+class IndustryTranslationOptions(TranslationOptions):
+    fields = ('title', )
+
